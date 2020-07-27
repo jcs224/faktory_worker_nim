e4b5d0a6200321ef8bc68e16b4eee381d3a70080
Joe Sweeney <joe.sweeney224@gmail.com>
2020-07-27 14:00:37 -0600

    trying to get SHA256 hash loop working

---
+++ .nimble
@@ -0,0 +1,1 @@
+requires "nimsha2 = 0.1.1"
--- main.nim
+++ main.nim
@@ -1,5 +1,7 @@
 import net
 import strutils
+import json
+import nimSHA2
 
 proc readLine(socket: Socket): string =
   var inString = socket.recv(1)
@@ -7,7 +9,35 @@ proc readLine(socket: Socket): string =
     inString.add(socket.recv(1))
   inString
 
+proc writeLine(socket: Socket, stringPayload: string): string =
+  socket.send(stringPayload)
+  socket.readLine()
+
 var socket = newSocket()
-socket.connect("localhost", Port(7419))
-echo socket.readLine()
+socket.connect("localhost", Port(7421))
+# Get initial connection response
+let response = socket.readLine()
+let responseJSONString = response[response.find("{") .. response.rfind("}")]
+echo responseJSONString
+
+# Parse JSON response
+let responseJSON = parseJson(responseJSONString)
+
+# Submit password if needed
+echo responseJSON["s"].getStr()
+echo responseJSON["i"].getInt()
+let iter = responseJSON["i"].getInt()
+let seed = responseJSON["s"].getStr()
+var data = "password" & seed
+
+# for i in 0 ..< iter:
+  # something here
+  
+let finalHex = "12345"
+echo finalHex
+
+let echoString = "HELLO {\"v\":2,\"pwdhash\":\"" & finalHex & "\"}\r\n"
+echo echoString
+echo socket.writeLine(echoString)
+
 socket.close()
