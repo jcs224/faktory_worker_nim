c10556377c5635a1891a8f308dee1f4c4c477271
Joe Sweeney <joe.sweeney224@gmail.com>
2020-07-28 11:53:22 -0600

    Create Type for FacktoryClient

--- main.nim
+++ main.nim
@@ -13,30 +13,39 @@ proc writeLine(socket: Socket, stringPayload: string): string =
   socket.send(stringPayload)
   socket.readLine()
 
-var socket = newSocket()
-socket.connect("localhost", Port(7421))
-# Get initial connection response
-let response = socket.readLine()
-let responseJSONString = response[response.find("{") .. response.rfind("}")]
+type
+  FaktoryClient = object
+    host: string
+    port: int
+    password: string
 
-# Parse JSON response
-let responseJSON = parseJson(responseJSONString)
-var outJSON = %* {"v":2}
+proc connect(client: var FaktoryClient): Socket =
+  var socket = newSocket()
+  socket.connect(client.host, Port(client.port))
+  let response = socket.readLine()
+  let responseJSONString = response[response.find("{") .. response.rfind("}")]
 
-# Submit password if needed
-if not isNil(responseJSON{"i"}) and not isNil(responseJSON{"s"}):
-  let iter = responseJSON["i"].getInt()
-  let seed = responseJSON["s"].getStr()
-  var initAuthData = "password" & seed
-  var authData = computeSHA256(initAuthData)
+  # Parse JSON response
+  let responseJSON = parseJson(responseJSONString)
+  var outJSON = %* {"v":2}
 
-  for i in 1 ..< iter:
-    authData = computeSHA256($authData)
+  # Submit password if needed
+  if not isNil(responseJSON{"i"}) and not isNil(responseJSON{"s"}):
+    let iter = responseJSON["i"].getInt()
+    let seed = responseJSON["s"].getStr()
+    var initAuthData = client.password & seed
+    var authData = computeSHA256(initAuthData)
 
-  let finalHex = toLowerAscii(authData.toHex())
-  outJSON["pwdhash"] = %finalHex
+    for i in 1 ..< iter:
+      authData = computeSHA256($authData)
 
-let echoString = "HELLO " & $outJSON & "\r\n"
-echo socket.writeLine(echoString)
+    let finalHex = toLowerAscii(authData.toHex())
+    outJSON["pwdhash"] = %finalHex
 
-socket.close()
+  let echoString = "HELLO " & $outJSON & "\r\n"
+  echo socket.writeLine(echoString)
+  result = socket
+
+var fakClient = FaktoryClient(host: "localhost", port: 7419, password: "")
+let fakSocket = fakClient.connect()
+fakSocket.close()
